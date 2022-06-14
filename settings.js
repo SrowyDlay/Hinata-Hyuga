46b9bc24ae725e95d938cadbebbdeac18c93ae70
SrowyDlay <61784424+SrowyDlay@users.noreply.github.com>
2022-06-14 22:41:14 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
+  global.ownername = 'E L L Z Y  ツ☂' // Ganti Jadi Namamu
+  global.owner = ['6285788029733','6285788029733','6285788029733'] // Kalo Mau Lebih Tambahin Aja
   global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
+  global.botname = 'Selly Botz' // Ganti Jadi Nama Botmu
+  global.packname = 'Selly Botz' // Sticker Weem
+  global.author = 'E L L Z Y  ツ☂' // Sticker Weem
   global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.linkgroup = 'https://chat.whatsapp.com/F1LkEvvm4CgHsi1g2ImaDv' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = '-' // Ganti Sama Link Yt Mu
+  global.github = '-' // Ganti Sama Link Github Mu
+  global.instagram = '-' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -56,8 +56,8 @@
 
   // Edit Sesuai Kemauanmu
   global.mess = {
-  admin: '_Lu Admin? 🤨_',
-  botAdmin: '_Bot Bukan Admin 😔_',
+  admin: '_Ellzy gsnteng? _',
+  botAdmin: '_yo'i _',
   owner: '_Lu Bukan Owner Gw 😠_',
   group: '_Khusus Group 😏_',
   private: '_Khusus Private Chat_',
